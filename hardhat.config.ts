9a52d8451b982330788a6f5699056892ae88fdaf
HardCoder <bitlabs27@gmail.com>
2023-03-02 19:22:12 +0900

    add hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from 'dotenv';
+import { resolve } from 'path';
+dotenvConfig({ path: resolve(__dirname, './.env') });
+
+
+
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+
+const CHAIN_IDS = {
+  hardhat: 1337,
+  mainnet: 1,
+  ropsten: 3,
+  rinkeby: 4,
+  bscmain: 56,
+  bsctest: 97
+};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,25 @@
-import { ethers } from "hardhat";
+/* eslint-disable no-console */
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+// Addresses in BSCtest
+import * as TOKENBSC from '../deployments/bsctest/TokenBSC.json';
 
+/**
+ * main function
+ */
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const TokenBSC = await ethers.getContractFactory('TokenBSC');
+  const tokenBSC = await TokenBSC.attach(TOKENBSC.address);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const [owner] = await ethers.getSigners();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const balance = await tokenBSC.balanceOf(owner.address);
+  console.log(`Balance of ${owner.address} in BSCTest: ${balance}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
